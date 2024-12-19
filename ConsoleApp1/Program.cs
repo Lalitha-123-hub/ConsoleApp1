db11e0a060cf43948121479ebc9c7d158912f558
dell <dell@DESKTOP-KB6FBG3>
2024-12-19 12:39:54 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(byte.MinValue + "  " + byte.MaxValue);
+            Console.WriteLine(sbyte.MinValue + "  " + sbyte.MaxValue);
+            Console.WriteLine(short.MinValue + "  " + short.MaxValue);
+            Console.WriteLine(ushort.MinValue + "  " + ushort.MaxValue);
+            Console.WriteLine(int.MinValue + "  " + int.MaxValue);
+            Console.WriteLine(uint.MinValue + "  " + uint.MaxValue);
+            Console.WriteLine(long.MinValue + "  " + long.MaxValue);
+            Console.WriteLine(ulong.MinValue + "  " + ulong.MaxValue);
+
+            // Explicit TC - Parse()
+            //string x = "300";
+            //int y = int.Parse(x);
+            //Console.WriteLine(x + "  " + y);
+        }
+    }
+}
